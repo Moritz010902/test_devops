7c6900436aad837b6e80f5b87c0f7dfc1bcb56a4
Moritz010902 <moritz.neumann02@gmail.com>
2025-06-07 00:16:18 +0200

    feat!: change preset

--- release.config.js
+++ release.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   // Explizite Plugin-Liste OHNE @semantic-release/npm
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "conventionalcommits"
+      }
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/github"
   ]
